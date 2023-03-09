ee46b7c98cba3f4a91c10fbf5273b18a86f86f46
Nguyen Thanh Hiep <nthiep1998@gmail.com>
2023-03-09 16:59:59 +0700

    ac

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -1,6 +1,5 @@
 package com.devsecops;
 
-import org.junit.*;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
