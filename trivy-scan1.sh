6d05c70bbadae25cd53cd2f8a24f4d3b6a983ebb
hiep98 <nthiep1998>
2023-03-27 21:49:45 +0700

    trivy 1 again

--- trivy-scan1.sh
+++ trivy-scan1.sh
@@ -5,7 +5,7 @@ echo $dockerImageName
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.38.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.38.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
-    # Trivy scan result processing
+# Trivy scan result processing
     exit_code=$?
     echo "Exit Code : $exit_code"
 
