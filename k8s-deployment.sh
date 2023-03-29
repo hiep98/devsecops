c926c9c9f1de132c24028a6f9a3ece8596621c8f
hiep98 <nthiep1998>
2023-03-29 14:34:45 +0700

    back roll

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 #k8s-deployment.sh
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
-kubectl -n default get deployment ${deploymentName} > /dev/null
-
-if [[ $? -ne 0 ]]; then
-    echo "deployment ${deploymentName} doesn't exist"
-    kubectl -n default apply -f k8s_deployment_service.yaml
-else
-    echo "deployment ${deploymentName} exists"
-    echo "image name - ${imageName}"
-    kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
-fi
-
-if [[ $(find k8s_deployment_service.yaml -mmin -1) ]] || [[ $? -ne 0 ]]; then
-    echo "k8s_deployment_service.yaml has been updated in the last minute or the previous command failed"
-    kubectl -n default apply -f k8s_deployment_service.yaml
-else
-    echo "k8s_deployment_service.yaml has not been updated in the last minute and the previous command was successful"
-fi
-
+kubectl -n default apply -f k8s_deployment_service.yaml
 
 kdic=$(kubectl diff -f k8s_deployment_service.yaml)
 if [[ $(kubectl diff -f k8s_deployment_service.yaml) ]]; then
