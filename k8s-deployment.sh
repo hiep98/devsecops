a04b4a9a456150746c3b0ce7a7c79a936dc12e49
hiep98 <nthiep1998>
2023-03-29 15:00:20 +0700

    pod info

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -9,3 +9,15 @@ if [[ $(kubectl diff -f k8s_deployment_service.yaml) ]]; then
 else
     echo "$kdic exist"
 fi
+
+
+
+
+# Get all pod names in current namespace
+pods=$(kubectl get pods -o jsonpath='{.items[*].metadata.name}')
+
+# Loop through each pod and describe its events
+for pod in $pods; do
+  echo "Getting events for pod: $pod"
+  kubectl get events --field-selector involvedObject.name=$pod --all-namespaces | awk -v var="$pod" '$0 ~ var'
+done
