612311b5ad7dc8156256664ee87c753fcb2d81fb
hiep98 <nthiep1998>
2023-04-01 18:58:45 +0700

    update springboot 2.5.12

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -5,7 +5,7 @@
 echo $imageName #getting Image name from env variable
 
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:latest -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:latest -q image --exit-code 0 --severity CRITICAL --light $imageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:latest -q image --exit-code 1 --severity CRITICAL --light $imageName
 
     # Trivy scan result processing
     exit_code=$?
