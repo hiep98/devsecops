a728a8603d4e83b1c424b4c735e88e5611da9d96
hiep98 <nthiep1998>
2023-04-05 21:13:32 +0700

    add exception

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -2,6 +2,10 @@ package com.devsecops;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,20 +13,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @RestController
-@RequestMapping("/api")
 public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private static final String baseURL = "http://node-service:5000/plusone";
 	
 	RestTemplate restTemplate = new RestTemplate();
-}
 	
-public class compare {
+	@RestController
+	public class compare {
 
 		@GetMapping("/")
 		public String welcome() {
@@ -49,3 +50,37 @@ public class compare {
 			return Integer.parseInt(response);
 		}
 	}
+
+}
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
+        // log the error message
+        log.error("Unexpected error occurred: {}", ex.getMessage());
+
+        // create a custom error response
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An unexpected error occurred");
+
+        return handleExceptionInternal(ex, errorResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    @ExceptionHandler(YourCustomException.class)
+    public ResponseEntity<Object> handleCustomException(YourCustomException ex, WebRequest request) {
+        // log the error message with the specific details
+        log.error("Your custom exception occurred: {}", ex.getMessage());
+
+        // create a custom error response with the specific details
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+
+        return handleExceptionInternal(ex, errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
+
+public class ErrorResponse {
+    private int status;
+    private String message;
+
+    // constructor, getters and setters
+}
