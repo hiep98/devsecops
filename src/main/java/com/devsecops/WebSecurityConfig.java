27cececce6e5f92fb0503622b4c5160fbd288d93
hiep98 <nthiep1998>
2023-04-06 18:40:16 +0700

    csrfTokenRepository

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -9,7 +9,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().csrfTokenRepository(csrfTokenRepository());
+        #http.csrf().disable();
 
     }
 }
